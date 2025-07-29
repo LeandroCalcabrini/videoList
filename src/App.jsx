1aa0cc4b4255af4c721f9b5f61fb15a858ea7268
Leandro <leacalca@gmail.com>
2025-07-29 18:21:03 -0300

    agregado de paginacion y contexto en la app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,43 @@
-
-import './App.css'
-import VideoGamesList from './components/VideoGamesList'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import User from "./pages/User";
+import { contextApp } from "./context/contextApp";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [videoGames, setVideoGames] = useState([]);
+  const [error, setError] = useState(null);
+  const apiGames = async () => {
+    try {
+      const url = import.meta.env.VITE_API_URL;
+      const resp = await fetch(url);
+      const data = await resp.json();
+      console.log(data);
+      setVideoGames(data.results);
+    } catch (error) {
+      setError("Problema al realizar el fetch de la api");
+      console.error(error);
+    }
+  };
 
+  useEffect(() => {
+    apiGames();
+  }, []);
 
+  console.log(videoGames);
   return (
     <>
-    <VideoGamesList/>
+      <contextApp.Provider value={{ error, setError }}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/user" element={<User />} />
+          </Routes>
+        </BrowserRouter>
+      </contextApp.Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/VideoGamesList.jsx
+++ src/components/VideoGamesList.jsx
@@ -1,33 +1,13 @@
 import { useEffect, useState } from "react";
-import "../components/VideoGamesList.css"
+import "../components/VideoGamesList.css";
 
 const VideoGamesList = () => {
-  const [videoGames, setVideoGames] = useState([]);
-  const [error, setError] = useState(null);
-  const apiGames = async () => {
-    try {
-      const url = import.meta.env.VITE_API_URL;
-      const resp = await fetch(url);
-      const data = await resp.json();
-      console.log(data)
-      setVideoGames(data.results)
-    } catch (error) {
-    setError("Problema al realizar el fetch de la api");
-    console.error(error)
-      
-    }
-  };
-
-  useEffect(() => {
-    apiGames();
-  }, []);
-
-  console.log(videoGames)
+  
   return (
-    <>
-    </>
-  )
-
+    <section className="container">
+      <div className="cards-container"></div>
+    </section>
+  );
 };
 
 export default VideoGamesList;
